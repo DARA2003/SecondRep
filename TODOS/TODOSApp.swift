6a52ab58dd06942d39d4e6577196cf666977c051
Mai Huy Vu <huyvu06092003@gmail.com>
2025-12-12 05:52:30 +0700

    Initial Commit

---
+++ TODOS/TODOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOSApp.swift
+//  TODOS
+//
+//  Created by Mai huy vu on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TODOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
